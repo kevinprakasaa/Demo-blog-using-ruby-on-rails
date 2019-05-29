3878be577a922ed10593e621b6ea414183431c1c
Prakasa <kevinprakasa15@gmail.com>
2019-05-28 18:37:48 -0700

    Finish implementation of blog functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,50 @@
 class PostsController < ApplicationController
 
-  def index
+  def index #function to show all posts
+    @posts = Post.all.order("created_at DESC")
   end
 
-  def new
+  def new #function to create a new post
+    @post = Post.new
   end
 
-  def create
+  def create #function to create a new post
+    @post = Post.new(post_params)
+    if(@post.save)
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
+  def show #function to show post
+    @post = Post.find(params[:id])
+  end
+
+  def update #function to update the post
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def edit #function to edit the post
+    @post = Post.find(params[:id])
+  end
+
+  def destroy #function to delete the post
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  private
+  def post_params #declaring params for the post
+    params.require(:post).permit(:title, :content)
+  end
 
 end
